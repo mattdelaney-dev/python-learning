c8cab82e06cef63cf4eba6715876add4a69fda5c
Matthew Delaney <matt.delaney.dev@gmail.com>
2026-06-27 17:58:39 +1000

    Completed Day 27

--- day1-100/day-27/main.py
+++ day1-100/day-27/main.py
@@ -1,12 +1,30 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("First GUI program!")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 # Label
+my_label = Label(text="I Am a Label", font=("Arial", 24))
+my_label.config(text="New Text", padx=20, pady=20)
+my_label.grid(column=0, row=0)
 
-my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24))
-my_label.pack(side="left")
+# Button Action
+def button_clicked():
+    new_text = entry.get()
+    my_label.config(text=new_text)
+
+# Button
+button = Button(text="Click Me!", command=button_clicked)
+button.grid(column=1, row=1)
+
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+# Entry
+entry = Entry(width=10)
+entry.grid(column=3, row=2)
+
 
 window.mainloop()
